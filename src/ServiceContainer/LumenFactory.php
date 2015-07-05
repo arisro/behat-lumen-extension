4ccf5fc89dfed6611f79bea954cea19817beef00
Buzachis Aris <buzachis.aris@gmail.com>
2015-07-05 23:07:07 +0300

    Initial commit.

---
+++ src/ServiceContainer/LumenFactory.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Arisro\Behat\ServiceContainer;
+
+use Behat\MinkExtension\ServiceContainer\Driver\DriverFactory;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class LumenFactory implements DriverFactory
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDriverName()
+    {
+        return 'lumen';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsJavascript()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configure(ArrayNodeDefinition $builder)
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildDriver(array $config)
+    {
+        $this->assertBrowserkitIsAvailable();
+
+        return new Definition('Arisro\Behat\Driver\KernelDriver', [
+            new Reference('lumen.app'),
+            '%mink.base_url%'
+        ]);
+    }
+
+    /**
+     * Ensure that BrowserKit is available.
+     *
+     * @throws RuntimeException
+     */
+    private function assertBrowserkitIsAvailable()
+    {
+        if ( ! class_exists('Behat\Mink\Driver\BrowserKitDriver')) {
+            throw new RuntimeException(
+                'Install MinkBrowserKitDriver in order to use the lumen driver.'
+            );
+        }
+    }
+
+}
+
